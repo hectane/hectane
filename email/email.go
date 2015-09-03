c5b007acade038db55ec34d760083b9fd2021a93
Nathan Osman <nathan@quickmediasolutions.com>
2015-09-03 16:29:33 -0700

    Finished implementing Email type.

--- email/email.go
+++ email/email.go
@@ -1,7 +1,10 @@
 package email
 
 import (
+	"code.google.com/p/go-uuid/uuid"
+
 	"bytes"
+	"fmt"
 	"mime/multipart"
 	"net/mail"
 	"net/textproto"
@@ -43,6 +46,17 @@ func createMultipartBody(text, html string) ([]byte, string) {
 	return buff.Bytes(), writer.Boundary()
 }
 
+// Create a message from the specified headers and data.
+func createMessage(hdrs map[string]string, body []byte) []byte {
+	buff := &bytes.Buffer{}
+	for hdr := range hdrs {
+		buff.Write([]byte(fmt.Sprintf("%s: %s\r\n", hdr, hdrs[hdr])))
+	}
+	buff.Write([]byte("\r\n"))
+	buff.Write(body)
+	return buff.Bytes()
+}
+
 // Attempt to extract the host from the provided email address.
 func hostFromAddress(data string) (string, error) {
 	if addr, err := mail.ParseAddress(data); err != nil {
@@ -66,4 +80,74 @@ func groupAddressesByHost(addrs []string) (map[string][]string, error) {
 		}
 	}
 	return m, nil
+}
+
+// Create an array of emails from the specified information. An email will be
+// generated for each individual host and for each BCC recipient.
+func NewEmails(from string, to, cc, bcc []string, subject string, text, html string) ([]*Email, error) {
+
+	// Create the list of emails that will be returned, build a map of headers,
+	// and generate the message body - it will be identical for all Email
+	// instances returned
+	var (
+		emails         = make([]*Email, 0, 1)
+		body, boundary = createMultipartBody(text, html)
+		id             = uuid.New()
+		hdrs           = map[string]string{
+			"Message-ID":   id,
+			"From":         from,
+			"To":           strings.Join(to, ","),
+			"Subject":      subject,
+			"Content-Type": fmt.Sprintf("multipart/alternative; boundary=\"%s\"", boundary),
+		}
+	)
+
+	// Add Cc addresses if any were provided
+	if len(cc) > 0 {
+		hdrs["Cc"] = strings.Join(cc, ",")
+	}
+
+	// The first email will be sent to all "To" and "Cc" addresses and does not
+	// contain any of the Bcc addresses
+	addrMap, err := groupAddressesByHost(append(to, cc...))
+	if err != nil {
+		return nil, err
+	}
+
+	// Generate emails for each of the hosts
+	for host := range addrMap {
+		emails = append(emails, &Email{
+			Id:      id,
+			Host:    host,
+			From:    from,
+			To:      addrMap[host],
+			Message: createMessage(hdrs, body),
+		})
+	}
+
+	// Generate emails for each of the Bcc addresses
+	for _, addr := range bcc {
+
+		// Reset a couple of headers
+		id = uuid.New()
+		hdrs["Message-ID"] = id
+		hdrs["Bcc"] = addr
+
+		// Fetch the host
+		host, err := hostFromAddress(addr)
+		if err != nil {
+			return nil, err
+		}
+
+		// Add the email to the list
+		emails = append(emails, &Email{
+			Id:      id,
+			Host:    host,
+			From:    from,
+			To:      []string{addr},
+			Message: createMessage(hdrs, body),
+		})
+	}
+
+	return emails, nil
 }
