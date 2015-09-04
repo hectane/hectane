c0bcb57e2d4debb15d42879e9593b1c31db1528f
Nathan Osman <nathan@quickmediasolutions.com>
2015-09-03 20:34:37 -0700

    Updated main.go to use the new types.

--- main.go
+++ main.go
@@ -2,16 +2,34 @@ package main
 
 import (
 	"github.com/goji/httpauth"
+	"github.com/nathan-osman/go-cannon/api"
+	"github.com/nathan-osman/go-cannon/queue"
 	"github.com/zenazn/goji"
 	"github.com/zenazn/goji/bind"
+	"github.com/zenazn/goji/web"
 
 	"crypto/tls"
 	"flag"
 	"log"
+	"net/http"
 	"os"
 )
 
+// Global mail queue exposed to the API methods.
+var q *queue.Queue
+
+// Goji middleware to expose the mail queue to the API methods.
+func queueMiddleware(c *web.C, h http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		c.Env["queue"] = q
+		h.ServeHTTP(w, r)
+	}
+	return http.HandlerFunc(fn)
+}
+
 func main() {
+
+	// Storage for values supplied via command-line parameters
 	var (
 		tlsCert  string
 		tlsKey   string
@@ -25,21 +43,30 @@ func main() {
 		flag.Lookup("bind").DefValue = ":8025"
 	}
 
+	// Add command-line flags for each of the options and then parse them
 	flag.StringVar(&tlsCert, "tls-cert", "", "certificate for TLS")
 	flag.StringVar(&tlsKey, "tls-key", "", "private key for TLS")
 	flag.StringVar(&username, "username", "", "username for HTTP basic auth")
 	flag.StringVar(&password, "password", "", "password for HTTP basic auth")
 	flag.Parse()
 
-	goji.Get("/v1/version", Version)
-	goji.Post("/v1/send", Send)
+	// Create the mail queue
+	q = queue.NewQueue()
+	defer q.Stop()
 
-	// If username and password are provided, enable HTTP basic auth
+	// Add the queue middleware
+	goji.Use(queueMiddleware)
+
+	// Add the two current API methods
+	goji.Get("/v1/version", api.Version)
+	goji.Post("/v1/send", api.Send)
+
+	// If username and password were provided, enable HTTP basic auth
 	if username != "" && password != "" {
 		goji.Use(httpauth.SimpleBasicAuth(username, password))
 	}
 
-	// If a TLS certificate and key were provided, enable TLS
+	// If a TLS certificate and key were provided, enable TLS and serve
 	if tlsCert != "" && tlsKey != "" {
 		cert, err := tls.LoadX509KeyPair(tlsCert, tlsKey)
 		if err != nil {
