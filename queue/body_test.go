774a7bb3ed8d0ea67acf023e2cd7ddcdce37ac53
Nathan Osman <nathan@quickmediasolutions.com>
2015-09-16 11:56:16 -0600

    Added another test for Body.

--- queue/body_test.go
+++ queue/body_test.go
@@ -4,7 +4,9 @@ import (
 	"code.google.com/p/go-uuid/uuid"
 	"github.com/nathan-osman/go-cannon/util"
 
+	"io/ioutil"
 	"os"
+	"reflect"
 	"testing"
 )
 
@@ -29,19 +31,61 @@ func TestBodyRefCount(t *testing.T) {
 		if err := writer.Close(); err != nil {
 			t.Fatal(err)
 		}
-		expectExists(t, b.metadataFilename(), true)
-		expectExists(t, b.messageBodyFilename(), true)
 		if err := b.Add(); err != nil {
 			t.Fatal(err)
 		}
 		if b.m.RefCount != 1 {
-			t.Fatalf("%s != %s", b.m.RefCount, 1)
+			t.Fatalf("%d != 1", b.m.RefCount)
+		}
+		expectExists(t, b.metadataFilename(), true)
+		expectExists(t, b.messageBodyFilename(), true)
+		if err := b.Release(); err != nil {
+			t.Fatal(err)
+		}
+		if b.m.RefCount != 0 {
+			t.Fatalf("%d != 0", b.m.RefCount)
+		}
+		expectExists(t, b.metadataFilename(), false)
+		expectExists(t, b.messageBodyFilename(), false)
+	} else {
+		t.Fatal(err)
+	}
+}
+
+func TestLoadBody(t *testing.T) {
+	var (
+		directory = os.TempDir()
+		id        = uuid.New()
+		data      = []byte("test")
+	)
+	if b, writer, err := NewBody(directory, id); err == nil {
+		if _, err := writer.Write(data); err != nil {
+			t.Fatal(err)
+		}
+		if err := writer.Close(); err != nil {
+			t.Fatal(err)
+		}
+		if err := b.Add(); err != nil {
+			t.Fatal(err)
+		}
+	} else {
+		t.Fatal(err)
+	}
+	if b, err := LoadBody(directory, id); err == nil {
+		if r, err := b.Reader(); err == nil {
+			if d, err := ioutil.ReadAll(r); err == nil {
+				if !reflect.DeepEqual(d, data) {
+					t.Fatal("%v != %v", d, data)
+				}
+			} else {
+				t.Fatal(err)
+			}
+		} else {
+			t.Fatal(err)
 		}
 		if err := b.Release(); err != nil {
 			t.Fatal(err)
 		}
-		expectExists(t, b.metadataFilename(), false)
-		expectExists(t, b.messageBodyFilename(), false)
 	} else {
 		t.Fatal(err)
 	}
