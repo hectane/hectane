e91b834f3c935dd789edd3337e666b239bebf86e
Nathan Osman <nathan@quickmediasolutions.com>
2015-09-19 12:52:16 -0700

    Added incomplete test for Filestore.

--- util/file_test.go
+++ util/file_test.go
@@ -1,1 +1,25 @@
 package util
+
+import (
+	"code.google.com/p/go-uuid/uuid"
+
+	"os"
+	"path"
+	"testing"
+)
+
+func newFilestore() (*Filestore, error) {
+	directory := path.Join(os.TempDir(), uuid.New())
+	if err := os.MkdirAll(directory, 0755); err != nil {
+		return nil, err
+	}
+	return NewFilestore(directory)
+}
+
+func TestFilestore(t *testing.T) {
+	if f, err := newFilestore(); err == nil {
+		_ = f
+	} else {
+		t.Fatal(err)
+	}
+}
