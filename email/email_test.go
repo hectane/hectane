d5360753321f87dc5ebb8b8d3f70b7345ee19d56
Nathan Osman <nathan@quickmediasolutions.com>
2015-09-19 12:52:49 -0700

    Simplified Email test.

--- email/email_test.go
+++ email/email_test.go
@@ -7,25 +7,19 @@ import (
 
 var (
 	directory = os.TempDir()
-	from      = "a@example.com"
-	to        = "b@example.com"
-	cc        = "c@example.com"
-	bcc       = "d@example.org"
+	addr1A    = "1@a.com"
+	addr2A    = "2@a.com"
+	addr1B    = "1@b.com"
 	subject   = "Test"
 	text      = "test"
 	html      = "<em>test</em>"
-	e         = &Email{
-		From:    from,
-		To:      []string{to},
-		Cc:      []string{cc},
-		Bcc:     []string{bcc},
-		Subject: subject,
-		Text:    text,
-		Html:    html,
-	}
 )
 
 func TestEmailMessageCount(t *testing.T) {
+	e := &Email{
+		To: []string{addr1A, addr2A},
+		Cc: []string{addr1B},
+	}
 	if messages, err := e.Messages(directory); err == nil {
 		if len(messages) != 2 {
 			t.Fatalf("%d != 2", len(messages))
