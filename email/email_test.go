8a98313290925c6603ac7a49c9edeb2b46a5a9fc
Nathan Osman <nathan@quickmediasolutions.com>
2015-09-18 18:40:22 -0600

    Added Email test.

--- email/email_test.go
+++ email/email_test.go
@@ -5,32 +5,30 @@ import (
 	"testing"
 )
 
-func TestEmailMessages(t *testing.T) {
-	var (
-		directory = os.TempDir()
-		from      = "a@hotmail.com"
-		to        = "b@hotmail.com"
-		cc        = "c@hotmail.com"
-		bcc       = "d@yahoo.com"
-		subject   = "Test"
-		text      = "test"
-		html      = "<em>test</em>"
-		e         = &Email{
-			From:    from,
-			To:      []string{to},
-			Cc:      []string{cc},
-			Bcc:     []string{bcc},
-			Subject: subject,
-			Text:    text,
-			Html:    html,
-		}
-	)
+var (
+	directory = os.TempDir()
+	from      = "a@example.com"
+	to        = "b@example.com"
+	cc        = "c@example.com"
+	bcc       = "d@example.org"
+	subject   = "Test"
+	text      = "test"
+	html      = "<em>test</em>"
+	e         = &Email{
+		From:    from,
+		To:      []string{to},
+		Cc:      []string{cc},
+		Bcc:     []string{bcc},
+		Subject: subject,
+		Text:    text,
+		Html:    html,
+	}
+)
+
+func TestEmailMessageCount(t *testing.T) {
 	if messages, err := e.Messages(directory); err == nil {
-		hosts := make([]string, 0, 2)
-		for _, m := range messages {
-			_ = hosts
-			_ = m
-			//...
+		if len(messages) != 2 {
+			t.Fatalf("%d != 2", len(messages))
 		}
 	} else {
 		t.Fatal(err)
