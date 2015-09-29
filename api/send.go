7a8390e8be83c62f9b5cb44fbf9b32d045799419
Nathan Osman <nathan@quickmediasolutions.com>
2015-09-28 19:39:15 -0700

    Removed redundant check from API method.

--- api/send.go
+++ api/send.go
@@ -1,13 +1,11 @@
 package api
 
 import (
-	"github.com/kennygrant/sanitize"
 	"github.com/nathan-osman/go-cannon/email"
 	"github.com/nathan-osman/go-cannon/queue"
 	"github.com/zenazn/goji/web"
 
 	"encoding/json"
-	"html"
 	"net/http"
 )
 
@@ -17,11 +15,6 @@ func Send(c web.C, w http.ResponseWriter, r *http.Request) {
 	if err := json.NewDecoder(r.Body).Decode(&e); err != nil {
 		respondWithError(w, err.Error())
 	} else {
-		if e.Html == "" {
-			e.Html = html.EscapeString(e.Text)
-		} else if e.Text == "" {
-			e.Text = sanitize.HTML(e.Html)
-		}
 		s := c.Env["storage"].(*queue.Storage)
 		q := c.Env["queue"].(*queue.Queue)
 		if messages, err := e.Messages(s); err == nil {
