6af000daec5f5beb657fe71fc6185a1f5aee9e9b
Nathan Osman <nathan@quickmediasolutions.com>
2015-10-10 21:18:24 -0700

    Fixed API tests.

--- api/api_test.go
+++ api/api_test.go
@@ -4,22 +4,59 @@ import (
 	"fmt"
 	"net/http"
 	"testing"
+	"time"
 )
+
+func getStatusCode(url, username, password string) (int, error) {
+	if r, err := http.NewRequest("GET", url, nil); err == nil {
+		if username != "" && password != "" {
+			r.SetBasicAuth(username, password)
+		}
+		if resp, err := http.DefaultClient.Do(r); err == nil {
+			return resp.StatusCode, nil
+		} else {
+			return 0, err
+		}
+	} else {
+		return 0, err
+	}
+}
 
 func TestBasicAuth(t *testing.T) {
 	var (
-		username = "test"
-		password = "test"
-		config   = &Config{
-			Addr:     "127.0.0.1:0",
+		addr        = "127.0.0.1:9000"
+		url         = fmt.Sprintf("http://%s/v1/version", addr)
+		username    = "test"
+		password    = "test"
+		badPassword = "test2"
+		testCases   = []struct {
+			username   string
+			password   string
+			statusCode int
+		}{
+			{"", "", http.StatusUnauthorized},
+			{username, badPassword, http.StatusForbidden},
+			{username, password, http.StatusOK},
+		}
+		config = &Config{
+			Addr:     addr,
 			Username: username,
 			Password: password,
 		}
 		a = New(config, nil)
 	)
-	if resp, err := http.Get(fmt.Sprintf("http://%s/v1/version", addr)); err == nil {
-		t.Log(resp)
-	} else {
-		t.Fatal(err)
+	go func() {
+		a.Listen()
+	}()
+	time.Sleep(50 * time.Millisecond)
+	defer a.Stop()
+	for _, c := range testCases {
+		if s, err := getStatusCode(url, c.username, c.password); err == nil {
+			if s != c.statusCode {
+				t.Fatalf("%d != %d", s, c.statusCode)
+			}
+		} else {
+			t.Fatal(err)
+		}
 	}
 }
