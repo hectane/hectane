43eab300604066ee69539d0f4beb4bc656d4e0ae
Nathan Osman <nathan@quickmediasolutions.com>
2015-10-24 12:43:31 -0700

    Added exec package to main.go.

--- main.go
+++ main.go
@@ -2,44 +2,34 @@ package main
 
 import (
 	"github.com/hectane/hectane/api"
+	"github.com/hectane/hectane/exec"
 	"github.com/hectane/hectane/queue"
 
 	"flag"
 	"log"
-	"os"
-	"os/signal"
-	"syscall"
 )
 
 func main() {
 	var (
-		filename = flag.String("config", "", "file containing configuration")
 		config   Config
+		filename = flag.String("config", "", "file containing configuration")
 	)
 	config.RegisterFlags()
 	flag.Parse()
 	if *filename != "" {
 		if err := config.LoadFromFile(*filename); err != nil {
-			log.Println(err)
-			os.Exit(1)
+			log.Fatal(err)
 		}
 	}
-	if q, err := queue.NewQueue(&config.Queue); err == nil {
-		defer q.Stop()
-		var (
-			a = api.New(&config.API, q)
-			c = make(chan os.Signal)
-		)
-		if err = a.Start(); err == nil {
-			defer a.Stop()
-		} else {
-			log.Println(err)
-			os.Exit(1)
-		}
-		signal.Notify(c, syscall.SIGINT)
-		<-c
-	} else {
-		log.Println(err)
-		os.Exit(1)
+	q, err := queue.NewQueue(&config.Queue)
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer q.Stop()
+	a := api.New(&config.API, q)
+	if err = a.Start(); err != nil {
+		log.Fatal(err)
+	}
+	defer a.Stop()
+	exec.Exec()
 }
