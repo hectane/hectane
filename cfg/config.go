f92480be44fa7e6acabdc36003a2fa3b7c5441f4
Nathan Osman <nathan@quickmediasolutions.com>
2015-10-26 15:17:12 -0700

    Added Config.Save() method.

--- cfg/config.go
+++ cfg/config.go
@@ -19,16 +19,16 @@ type Config struct {
 // Parse the flags passed to the application
 func Parse() (*Config, error) {
 	var (
-		cfg      = &Config{}
+		c        = &Config{}
 		filename = flag.String("config", "", "file containing configuration")
 	)
-	flag.StringVar(&cfg.API.Addr, "bind", ":8025", "`address` and port to bind to")
-	flag.StringVar(&cfg.API.TLSCert, "tls-cert", "", "certificate `file` for TLS")
-	flag.StringVar(&cfg.API.TLSKey, "tls-key", "", "private key `file` for TLS")
-	flag.StringVar(&cfg.API.Username, "username", "", "`username` for HTTP basic auth")
-	flag.StringVar(&cfg.API.Password, "password", "", "`password` for HTTP basic auth")
-	flag.StringVar(&cfg.Queue.Directory, "directory", path.Join(os.TempDir(), "hectane"), "`directory` for persistent storage")
-	flag.BoolVar(&cfg.Queue.DisableSSLVerification, "disable-ssl-verification", false, "don't verify SSL certificates")
+	flag.StringVar(&c.API.Addr, "bind", ":8025", "`address` and port to bind to")
+	flag.StringVar(&c.API.TLSCert, "tls-cert", "", "certificate `file` for TLS")
+	flag.StringVar(&c.API.TLSKey, "tls-key", "", "private key `file` for TLS")
+	flag.StringVar(&c.API.Username, "username", "", "`username` for HTTP basic auth")
+	flag.StringVar(&c.API.Password, "password", "", "`password` for HTTP basic auth")
+	flag.StringVar(&c.Queue.Directory, "directory", path.Join(os.TempDir(), "hectane"), "`directory` for persistent storage")
+	flag.BoolVar(&c.Queue.DisableSSLVerification, "disable-ssl-verification", false, "don't verify SSL certificates")
 	flag.Parse()
 	if *filename != "" {
 		r, err := os.Open(*filename)
@@ -36,9 +36,22 @@ func Parse() (*Config, error) {
 			return nil, err
 		}
 		defer r.Close()
-		if err = json.NewDecoder(r).Decode(cfg); err != nil {
+		if err = json.NewDecoder(r).Decode(c); err != nil {
 			return nil, err
 		}
 	}
-	return cfg, nil
+	return c, nil
+}
+
+// Save the configuration to the specified path.
+func (c *Config) Save(path string) error {
+	w, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer w.Close()
+	if err := json.NewEncoder(w).Encode(c); err != nil {
+		return err
+	}
+	return nil
 }
