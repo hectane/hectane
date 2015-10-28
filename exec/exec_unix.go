2ef718dabb420f60dc33600a763d5585423c4718
Nathan Osman <nathan@quickmediasolutions.com>
2015-10-27 22:39:42 -0700

    Added empty stubs for exec functions on Unix.

--- exec/exec_unix.go
+++ exec/exec_unix.go
@@ -2,8 +2,17 @@
 
 package exec
 
+// No initialization required.
+func Init() error {
+	return nil
+}
+
 // Run the application until terminated by a signal.
 func Exec() error {
 	execSignal()
 	return nil
+}
+
+// No cleanup necessary.
+func Cleanup() {
 }
