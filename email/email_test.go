f58c6aa38ca7a81591ee19dcea0f8725ef26c36e
Nathan Osman <nathan@quickmediasolutions.com>
2015-11-05 14:57:16 -0800

    Reformatted email tests.

--- email/email_test.go
+++ email/email_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/hectane/hectane/queue"
 
 	"bytes"
+	"errors"
 	"io/ioutil"
 	"net/mail"
 	"os"
@@ -12,42 +13,41 @@ import (
 )
 
 func emailToMessages(e *Email) ([]*queue.Message, []byte, error) {
-	if d, err := ioutil.TempDir(os.TempDir(), ""); err == nil {
-		defer os.RemoveAll(d)
-		s := queue.NewStorage(d)
-		if messages, err := e.Messages(s); err == nil {
-			if len(messages) > 0 {
-				if r, err := s.GetMessageBody(messages[0]); err == nil {
-					if b, err := ioutil.ReadAll(r); err == nil {
-						return messages, b, nil
-					} else {
-						return nil, nil, err
-					}
-				} else {
-					return nil, nil, err
-				}
-			} else {
-				return nil, nil, err
-			}
-		} else {
-			return nil, nil, err
-		}
+	d, err := ioutil.TempDir(os.TempDir(), "")
+	if err != nil {
+		return nil, nil, err
+	}
+	defer os.RemoveAll(d)
+	s := queue.NewStorage(d)
+	m, err := e.Messages(s)
+	if err != nil {
+		return nil, nil, err
+	}
+	if len(m) < 1 {
+		return nil, nil, errors.New("no messages")
+	}
+	r, err := s.GetMessageBody(m[0])
+	if err != nil {
+		return nil, nil, err
+	}
+	if b, err := ioutil.ReadAll(r); err == nil {
+		return m, b, nil
 	} else {
 		return nil, nil, err
 	}
 }
 
 func TestEmailCount(t *testing.T) {
-	if messages, _, err := emailToMessages(&Email{
+	m, _, err := emailToMessages(&Email{
 		From: "me@example.com",
 		To:   []string{"1@a.com", "1@b.com"},
 		Cc:   []string{"2@a.com", "2@b.com"},
-	}); err == nil {
-		if len(messages) != 2 {
-			t.Fatalf("%d != 2", len(messages))
-		}
-	} else {
+	})
+	if err != nil {
 		t.Fatal(err)
+	}
+	if len(m) != 2 {
+		t.Fatalf("%d != 2", len(m))
 	}
 }
 
@@ -58,31 +58,31 @@ func TestEmailHeaders(t *testing.T) {
 		bcc     = "hidden@example.com"
 		subject = "Test"
 	)
-	if _, body, err := emailToMessages(&Email{
+	_, body, err := emailToMessages(&Email{
 		From:    from,
 		To:      []string{to},
 		Bcc:     []string{bcc},
 		Subject: subject,
-	}); err == nil {
-		r := bytes.NewBuffer(body)
-		if m, err := mail.ReadMessage(r); err == nil {
-			if v := m.Header.Get("From"); v != from {
-				t.Fatalf("%s != %s", v, from)
-			}
-			if v := m.Header.Get("To"); v != to {
-				t.Fatalf("%s != %s", v, to)
-			}
-			if v := m.Header.Get("Bcc"); v != "" {
-				t.Fatalf("%s != \"\"", v)
-			}
-			if v := m.Header.Get("Subject"); v != subject {
-				t.Fatalf("%s != %s", v, subject)
-			}
-		} else {
-			t.Fatal(err)
-		}
-	} else {
+	})
+	if err != nil {
 		t.Fatal(err)
+	}
+	r := bytes.NewBuffer(body)
+	m, err := mail.ReadMessage(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if v := m.Header.Get("From"); v != from {
+		t.Fatalf("%s != %s", v, from)
+	}
+	if v := m.Header.Get("To"); v != to {
+		t.Fatalf("%s != %s", v, to)
+	}
+	if v := m.Header.Get("Bcc"); v != "" {
+		t.Fatalf("%s != \"\"", v)
+	}
+	if v := m.Header.Get("Subject"); v != subject {
+		t.Fatalf("%s != %s", v, subject)
 	}
 }
 
@@ -110,20 +110,20 @@ func TestEmailContent(t *testing.T) {
 			},
 		}
 	)
-	if _, body, err := emailToMessages(&Email{
+	_, body, err := emailToMessages(&Email{
 		From: from,
 		Text: text,
 		To:   []string{to},
-	}); err == nil {
-		r := bytes.NewBuffer(body)
-		if m, err := mail.ReadMessage(r); err == nil {
-			if err := assert.Multipart(m.Body, m.Header.Get("Content-Type"), description); err != nil {
-				t.Fatal(err)
-			}
-		} else {
-			t.Fatal(err)
-		}
-	} else {
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	r := bytes.NewBuffer(body)
+	m, err := mail.ReadMessage(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := assert.Multipart(m.Body, m.Header.Get("Content-Type"), description); err != nil {
 		t.Fatal(err)
 	}
 }
