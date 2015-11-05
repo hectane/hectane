5d93c845f5bac04cf9ceba03c1fe79aacb8ebae0
Nathan Osman <nathan@quickmediasolutions.com>
2015-11-05 15:41:13 -0800

    Moved final type from assert package to email tests.

--- email/email_test.go
+++ email/email_test.go
@@ -1,16 +1,57 @@
 package email
 
 import (
-	"github.com/hectane/hectane/assert"
+	"github.com/hectane/go-attest"
 	"github.com/hectane/hectane/queue"
 
 	"bytes"
 	"errors"
+	"fmt"
+	"io"
 	"io/ioutil"
+	"mime"
+	"mime/multipart"
 	"net/mail"
 	"os"
 	"testing"
 )
+
+// Description of a multipart MIME message.
+type multipartDesc struct {
+	ContentType string
+	Content     []byte
+	Parts       []*multipartDesc
+}
+
+// Ensure that a multipart message conforms to the specified description.
+func checkMultipart(r io.Reader, contentType string, d *multipartDesc) error {
+	c, params, err := mime.ParseMediaType(contentType)
+	if err != nil {
+		return err
+	}
+	if c != d.ContentType {
+		return fmt.Errorf("%s != %s", c, d.ContentType)
+	}
+	if len(d.Parts) == 0 {
+		return attest.Read(r, d.Content)
+	} else {
+		boundary, ok := params["boundary"]
+		if !ok {
+			return errors.New("\"boundary\" parameter missing")
+		}
+		reader := multipart.NewReader(r, boundary)
+		for _, part := range d.Parts {
+			p, err := reader.NextPart()
+			if err != nil {
+				return err
+			}
+			if err := checkMultipart(p, p.Header.Get("Content-Type"), part); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+}
 
 func emailToMessages(e *Email) ([]*queue.Message, []byte, error) {
 	d, err := ioutil.TempDir(os.TempDir(), "")
@@ -91,17 +132,17 @@ func TestEmailContent(t *testing.T) {
 		from        = "me@example.com"
 		to          = "you@example.com"
 		text        = "<test>\r\n"
-		description = &assert.MultipartDesc{
+		description = &multipartDesc{
 			ContentType: "multipart/mixed",
-			Parts: []*assert.MultipartDesc{
-				&assert.MultipartDesc{
+			Parts: []*multipartDesc{
+				&multipartDesc{
 					ContentType: "multipart/alternative",
-					Parts: []*assert.MultipartDesc{
-						&assert.MultipartDesc{
+					Parts: []*multipartDesc{
+						&multipartDesc{
 							ContentType: "text/plain",
 							Content:     []byte("<test>\r\n"),
 						},
-						&assert.MultipartDesc{
+						&multipartDesc{
 							ContentType: "text/html",
 							Content:     []byte("&lt;test&gt;<br>"),
 						},
@@ -123,7 +164,7 @@ func TestEmailContent(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if err := assert.Multipart(m.Body, m.Header.Get("Content-Type"), description); err != nil {
+	if err := checkMultipart(m.Body, m.Header.Get("Content-Type"), description); err != nil {
 		t.Fatal(err)
 	}
 }
