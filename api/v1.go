65369ea767f111dc332a1231e132537c47e0523d
Nathan Osman <nathan@quickmediasolutions.com>
2015-11-05 17:55:58 -0800

    Reformatted some code in the api package.

--- api/v1.go
+++ api/v1.go
@@ -17,55 +17,53 @@ type rawParams struct {
 // Send a raw MIME message.
 func (a *API) raw(r *http.Request) interface{} {
 	var p rawParams
-	if err := json.NewDecoder(r.Body).Decode(&p); err == nil {
-		if w, body, err := a.queue.Storage.NewBody(); err == nil {
-			if _, err := w.Write([]byte(p.Body)); err != nil {
-				return err
-			}
-			if err := w.Close(); err != nil {
-				return err
-			}
-			if hostMap, err := email.GroupAddressesByHost(p.To); err == nil {
-				for h, to := range hostMap {
-					m := &queue.Message{
-						Host: h,
-						From: p.From,
-						To:   to,
-					}
-					if err := a.queue.Storage.SaveMessage(m, body); err != nil {
-						return err
-					}
-					a.queue.Deliver(m)
-				}
-			} else {
-				return err
-			}
-		} else {
-			return err
-		}
-		return struct{}{}
-	} else {
+	if err := json.NewDecoder(r.Body).Decode(&p); err != nil {
 		return err
 	}
+	w, body, err := a.queue.Storage.NewBody()
+	if err != nil {
+		return err
+	}
+	if _, err := w.Write([]byte(p.Body)); err != nil {
+		return err
+	}
+	if err := w.Close(); err != nil {
+		return err
+	}
+	hostMap, err := email.GroupAddressesByHost(p.To)
+	if err == nil {
+		return err
+	}
+	for h, to := range hostMap {
+		m := &queue.Message{
+			Host: h,
+			From: p.From,
+			To:   to,
+		}
+		if err := a.queue.Storage.SaveMessage(m, body); err != nil {
+			return err
+		}
+		a.queue.Deliver(m)
+	}
+	return struct{}{}
 }
 
 // Send an email with the specified parameters.
 func (a *API) send(r *http.Request) interface{} {
 	var e email.Email
-	if err := json.NewDecoder(r.Body).Decode(&e); err == nil {
-		if messages, err := e.Messages(a.queue.Storage); err == nil {
-			for _, m := range messages {
-				a.queue.Deliver(m)
-			}
-			return struct{}{}
-		} else {
-			return map[string]string{
-				"error": err.Error(),
-			}
-		}
-	} else {
+	if err := json.NewDecoder(r.Body).Decode(&e); err != nil {
 		return err
 	}
+	messages, err := e.Messages(a.queue.Storage)
+	if err != nil {
+		return map[string]string{
+			"error": err.Error(),
+		}
+	}
+	for _, m := range messages {
+		a.queue.Deliver(m)
+	}
+	return struct{}{}
 }
 
 // Retrieve status information.
