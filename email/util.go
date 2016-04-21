3bebda85df90313310c751e57ba54ac94ec18a2c
Nathan Osman <nathan@quickmediasolutions.com>
2016-04-20 21:56:32 -0700

    Cut down on the number of "links" that the HTML conversion regex was finding.

--- email/util.go
+++ email/util.go
@@ -11,7 +11,7 @@ import (
 // The link RegExp is adapted from http://stackoverflow.com/a/3809435/193619.
 var (
 	replaceCRLF  = regexp.MustCompile(`\r?\n`)
-	replaceLinks = regexp.MustCompile(`(?:https?:\/\/)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&//=]*)`)
+	replaceLinks = regexp.MustCompile(`https?:\/\/[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&//=]*)`)
 )
 
 // Group a list of email addresses by their host. An error will be returned if
@@ -38,9 +38,6 @@ func toHTML(data string) string {
 	data = html.EscapeString(data)
 	data = replaceCRLF.ReplaceAllString(data, "<br>")
 	data = replaceLinks.ReplaceAllStringFunc(data, func(m string) string {
-		if !strings.HasPrefix(m, "http://") && !strings.HasPrefix(m, "https") {
-			m = fmt.Sprintf("http://%s", m)
-		}
 		return fmt.Sprintf("<a href=\"%s\">%s</a>", m, m)
 	})
 	return data
