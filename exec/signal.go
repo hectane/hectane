70c4c9d71992ea6961cd9ba18c9047bf582bf68a
Nathan Osman <nathan.osman@gmail.com>
2017-01-15 12:14:57 -0800

    Also terminate on SIGTERM.

--- exec/signal.go
+++ exec/signal.go
@@ -9,6 +9,6 @@ import (
 // Run until SIGINT is received.
 func execSignal() {
 	c := make(chan os.Signal)
-	signal.Notify(c, syscall.SIGINT)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
 	<-c
 }
