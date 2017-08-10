1218bea7666696ea3c91299caf02d0549cc4ff45
Nathan Osman <nathan@quickmediasolutions.com>
2017-08-10 14:00:58 -0700

    Enable Bootstrap in the UI.

---
+++ ui/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+global.jQuery = require('jquery');
+global.Tether = require('tether');
+
+require('bootstrap');
+require('bootstrap/dist/css/bootstrap.css');
