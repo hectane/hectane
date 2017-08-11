f1acf7b7c029b5e910035b1cc42be02c1fe4eb4d
Nathan Osman <nathan@quickmediasolutions.com>
2017-08-11 14:11:49 -0700

    Add action for creating an admin user.

--- cmd/hectane/main.go
+++ cmd/hectane/main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/hectane/hectane/db"
 	"github.com/hectane/hectane/server"
+	"github.com/howeyc/gopass"
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 )
+
+func initDB(c *cli.Context) error {
+	if err := db.Connect(
+		c.GlobalString("db-name"),
+		c.GlobalString("db-user"),
+		c.GlobalString("db-password"),
+		c.GlobalString("db-host"),
+		c.GlobalInt("db-port"),
+	); err != nil {
+		return err
+	}
+	if err := db.Migrate(); err != nil {
+		return err
+	}
+	return nil
+}
 
 func main() {
 	app := cli.NewApp()
@@ -51,60 +69,100 @@ func main() {
 			EnvVar: "DB_PASSWORD",
 			Usage:  "PostgreSQL database password",
 		},
-		cli.StringFlag{
-			Name:   "secret-key",
-			EnvVar: "SECRET_KEY",
-			Usage:  "key used for auth cookies",
-		},
-		cli.StringFlag{
-			Name:   "web-addr",
-			Value:  ":8000",
-			EnvVar: "WEB_ADDR",
-			Usage:  "address for the web interface",
-		},
 	}
-	app.Action = func(c *cli.Context) {
-
-		// Configure logging
-		log := logrus.WithField("context", "main")
+	app.Before = func(c *cli.Context) error {
 		if c.Bool("debug") {
 			logrus.SetLevel(logrus.DebugLevel)
 		}
-
-		// Connect to the database
-		if err := db.Connect(
-			c.String("db-name"),
-			c.String("db-user"),
-			c.String("db-password"),
-			c.String("db-host"),
-			c.Int("db-port"),
-		); err != nil {
-			log.Error(err)
-			return
-		}
-
-		// Perform all pending migrations
-		log.Info("performing pending migrations...")
-		if err := db.Migrate(); err != nil {
-			log.Error(err)
-			return
-		}
-
-		// Create the web server
-		s, err := server.New(&server.Config{
-			Addr:      c.String("web-addr"),
-			SecretKey: c.String("secret-key"),
-		})
-		if err != nil {
-			log.Error(err)
-			return
-		}
-		defer s.Close()
-
-		// Wait for SIGINT or SIGTERM
-		sigChan := make(chan os.Signal)
-		signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-		<-sigChan
+		return nil
 	}
-	app.Run(os.Args)
+	app.Commands = []cli.Command{
+		{
+			Name:  "createadmin",
+			Usage: "create an administrator user",
+			Action: func(c *cli.Context) error {
+
+				// Initialize the database
+				if err := initDB(c); err != nil {
+					return err
+				}
+
+				// Prompt for the username
+				var username string
+				fmt.Print("Username? ")
+				fmt.Scanln(&username)
+
+				// Prompt for the password, hiding the input
+				fmt.Print("Password? ")
+				p, err := gopass.GetPasswd()
+				if err != nil {
+					return err
+				}
+
+				// Generate a new user with the data
+				u := &db.User{
+					Username: username,
+					IsAdmin:  true,
+				}
+				if err := u.SetPassword(string(p)); err != nil {
+					return err
+				}
+
+				// Store the user in the database
+				if err := u.Save(db.DefaultToken); err != nil {
+					return err
+				}
+
+				return nil
+			},
+		},
+		{
+			Name:  "run",
+			Usage: "run the application",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:   "secret-key",
+					EnvVar: "SECRET_KEY",
+					Usage:  "key used for auth cookies",
+				},
+				cli.StringFlag{
+					Name:   "web-addr",
+					Value:  ":8000",
+					EnvVar: "WEB_ADDR",
+					Usage:  "address for the web interface",
+				},
+			},
+			Action: func(c *cli.Context) error {
+
+				// Initialize the database
+				if err := initDB(c); err != nil {
+					return err
+				}
+
+				// Create the web server
+				s, err := server.New(&server.Config{
+					Addr:      c.String("web-addr"),
+					SecretKey: c.String("secret-key"),
+				})
+				if err != nil {
+					return err
+				}
+				defer s.Close()
+
+				// Wait for SIGINT or SIGTERM
+				sigChan := make(chan os.Signal)
+				signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+				<-sigChan
+
+				return nil
+			},
+		},
+	}
+
+	// Watch for an error
+	log := logrus.WithField("context", "main")
+	if err := app.Run(os.Args); err != nil {
+		log.Error(err)
+		os.Exit(1)
+	}
 }
