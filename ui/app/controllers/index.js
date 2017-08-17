81c7fd0faa771e37ace21c694ae36841cf80ff49
Nathan Osman <nathan@quickmediasolutions.com>
2017-08-17 13:19:32 -0700

    Begin implementing home page.

---
+++ ui/app/controllers/index.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  session: Ember.inject.service('session'),
+  user: Ember.computed('session', function() {
+    return this.get('session.data.authenticated.user');
+  }),
+
+  actions: {
+    logout() {
+      this.get('session').invalidate();
+    }
+  }
+});
