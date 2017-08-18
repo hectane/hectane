ffbf10ac2e81ce747388b64860d28339a19e2ddd
Nathan Osman <nathan@quickmediasolutions.com>
2017-08-18 15:15:27 -0700

    Convert folder-sidebar to use new modal-dialog component.

--- ui/app/components/folder-sidebar.js
+++ ui/app/components/folder-sidebar.js
@@ -4,34 +4,19 @@ export default Ember.Component.extend({
   store: Ember.inject.service('store'),
 
   actions: {
-    showNewFolder() {
+    showNewDialog() {
       this.setProperties({
-        errorMessage: null,
-        newFolderName: ''
+        newFolderName: '',
+        newVisible: true
       });
-      Ember.$('#new-folder-modal')
-      .modal({
-        closable: false,
-        onApprove: () => {
-          let newFolderName = this.get('newFolderName');
-          let record = this.get('store').createRecord('folder', {
-            name: newFolderName
-          });
-          this.set('loading', true);
-          record.save()
-          .then(function() {
-            Ember.$('#new-folder-modal').modal('hide');
-          }, (message) => {
-            this.set('errorMessage', message);
-            record.deleteRecord();
-          })
-          .finally(() => {
-            this.set('loading', false);
-          });
-          return false;
-        }
-      })
-      .modal('show');
+    },
+
+    getNewPromise() {
+      let newFolderName = this.get('newFolderName');
+      let record = this.get('store').createRecord('folder', {
+        name: newFolderName
+      });
+      return record.save();
     }
   }
 });
