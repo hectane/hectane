aa7741d4a22fe7025b9f2c1710b26d89ab4ce689
Nathan Osman <nathan@quickmediasolutions.com>
2017-08-18 16:30:46 -0700

    Fix permissions on log page.

--- ui/app/routes/admin/users.js
+++ ui/app/routes/admin/users.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
-import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
+export default Ember.Route.extend({
   model() {
     return this.store.findAll('user');
   }
